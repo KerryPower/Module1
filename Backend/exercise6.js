637368fea1046eb29b318a390fcc0c2437f1bd26
KerryPower <kerrycpower@gmail.com>
2024-05-11 16:07:23 +1200

    Exercise 6

---
+++ Backend/exercise6.js
@@ -0,0 +1,18 @@
+// Exercise 6
+// Create and Modify JSON objects
+const book = {
+    "title": "The Curious Cat",
+    "description": "The Story of a Cat",
+    "author": "Humpfrey the Hippopotamus",
+    "pages": 212
+}
+
+console.log(book.title)
+console.log(book.description)
+console.log(book.author)
+console.log(book.pages)
+console.log(book)
+
+book.description= "Tales of heartache and woe, regaled from the point of view of a cat"
+
+console.log(book)
