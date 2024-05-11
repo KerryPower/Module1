5b9b59263e6fc39f4db250e0ddd3df7f66327a81
KerryPower <kerrycpower@gmail.com>
2024-05-11 14:55:58 +1200

    Exercise 4

---
+++ Backend/2varCalc.js
@@ -0,0 +1,33 @@
+//Exercise4.1
+//This outputs the calculation of 2 variables
+
+a = 10
+b = 2
+
+function division(a, b) {
+    return a / b
+}
+
+let divResult = division(a, b) 
+console.log("Divided: ", divResult)
+
+function multiplication(a, b){
+    return a * b
+}
+
+let multResult = multiplication(a, b) 
+console.log("Multiplied: ", multResult)
+
+function subtraction(a, b){
+    return a - b
+}
+
+let subResult = subtraction(a, b) 
+console.log("Subtracted: ", subResult)
+
+function addition(a, b){
+    return a + b
+}
+
+let addResult = addition(a, b) 
+console.log("Added: ", addResult)
---
+++ Backend/greetname.js
@@ -0,0 +1,7 @@
+// Exercise 4.2
+// This greets the name entered.
+
+let Greeting = "Hello"
+let Name = "Dave"
+
+console.log(Greeting + " " + Name)
