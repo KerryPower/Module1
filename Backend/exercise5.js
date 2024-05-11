d4007bc7975de4f0e7691b1206ad40c3fbb80d93
KerryPower <kerrycpower@gmail.com>
2024-05-11 15:35:47 +1200

    Exercise 5

---
+++ Backend/exercise5.js
@@ -0,0 +1,11 @@
+// Exercise 5 
+// Create and Modify Arrays
+
+let sports = ['Rugby','Football','Netball','Squash','Fencing']
+
+sports[1] = 'Cricket'
+sports[4] = 'Badminton'
+sports.unshift ('Hockey')
+sports.pop()
+
+console.log(sports)
